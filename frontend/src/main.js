9d5ebc6786e015a9ef7a01b819f9c7c001e78bc9
Gautam Mishra <mishragautam96@gmail.com>
2020-07-14 08:57:17 +0530

    add Backend_URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+
+const http = axios.create({
+  baseURL: process.env.BACKEND_URL ? process.env.BACKEND_URL : 'http://localhost:3000/api',
+});
+
+Vue.prototype.$http = http;
 
 new Vue({
   router,
