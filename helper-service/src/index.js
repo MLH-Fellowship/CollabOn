1f2fa704b1dadf06592922d66125c9dea3d95577
Gautam Mishra <mishragautam96@gmail.com>
2020-07-15 06:37:36 +0530

    cache apis to redis

--- helper-service/src/index.js
+++ helper-service/src/index.js
@@ -3,7 +3,8 @@ const { Octokit } = require("@octokit/rest");
 const redis = require('redis');
 const app = express();
 const port = 4000;
-const REDIS_PORT = 6379;
+
+const redis_client = redis.createClient({host: 'redis'});
 
 const octokit = new Octokit({
   userAgent: "myApp v1.2.3",
@@ -24,24 +25,47 @@ const octokit = new Octokit({
   }
 });
 
-const redis_client = redis.createClient(REDIS_PORT);
 
 app.get("/org/:org/info", (req, res) => {
   const org = req.params.org;
-  octokit.orgs.get({ org: org }).then(({ data }) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(data);
+  redis_client.get(org + '-info', (err, data) => {
+    if(err) console.log(err);
+
+    if(data==null) {
+      octokit.orgs.get({ org: org }).then(({ data }) => {
+        redis_client.set(String(org + '-info'), JSON.stringify(data), (err, redis_res) => {
+          if(err) console.log(err);
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.send(data);
+        });
+      });
+    } else {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.send(JSON.parse(data));
+    }
   });
 });
 
 app.get("/org/:org/members", (req, res) => {
   const org = req.params.org;
-  octokit.orgs
-    .listMembers({ org: org, per_page: 100, page: 1 })
-    .then(({ data }) => {
+  redis_client.get(org + '-members', (err, data) => {
+    if(err) console.log(err);
+
+    if(data==null) {
+      octokit.orgs
+        .listMembers({ org: org, per_page: 100, page: 1 })
+        .then(({ data }) => {
+          redis_client.set(String(org + '-members'), JSON.stringify(data), (err, redis_res) => {
+            if(err) console.log(err);
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.send(data);
+          });
+      });
+    } else {
       res.setHeader('Access-Control-Allow-Origin', '*');
-      res.send(data);
-    });
+      res.send(JSON.parse(data));
+    }
+  });
 });
 
 app.listen(port, () =>
